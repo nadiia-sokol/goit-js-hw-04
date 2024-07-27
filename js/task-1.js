4270c0dcde04214406db296121e905a9d810e217
Nadiia Sokol <dianadin@ukr.net>
2024-07-27 19:16:35 +0200

    ts-js

---
+++ js/task-1.js
@@ -0,0 +1,28 @@
+'use strict';
+function isEnoughCapacity(products, containerSize) {
+    const productValues = Object.values(products);
+    let sumProducts = 0;
+    for (const value of productValues){
+        sumProducts += value;
+        }
+    if (sumProducts <= containerSize) {
+            return true;
+        }
+        return false;
+};
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
